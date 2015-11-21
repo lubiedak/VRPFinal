2fd1c6feba4fe1af300ac7a183069a003f556419
lubiedak <l.biedak@wp.pl>
2015-11-21 14:25:17 +0000

    External yaml configuration - first steps

--- Backend/src/main/java/com/vrp/vrpBackend/service/impl/JenkinsServiceImpl.java
+++ Backend/src/main/java/com/vrp/vrpBackend/service/impl/JenkinsServiceImpl.java
@@ -3,6 +3,7 @@
  */
 package com.vrp.vrpBackend.service.impl;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,6 +14,7 @@ import com.vrp.vrpBackend.service.JenkinsService;
  *
  */
 @Component
+@ConfigurationProperties(prefix="jenkins")
 public class JenkinsServiceImpl implements JenkinsService {
 
 	public String triggerBuild() {
