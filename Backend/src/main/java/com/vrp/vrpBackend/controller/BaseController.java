e7986e3c8150287d4f0931caef0f6261cb187b6c
lubiedak <l.biedak@wp.pl>
2015-11-21 20:04:37 +0000

    HATEOAS - first try

--- Backend/src/main/java/com/vrp/vrpBackend/controller/BaseController.java
+++ Backend/src/main/java/com/vrp/vrpBackend/controller/BaseController.java
@@ -1,16 +1,28 @@
 package com.vrp.vrpBackend.controller;
 
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
 import org.springframework.hateoas.ResourceSupport;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-public abstract class BaseController extends ResourceSupport{
+import com.vrp.vrpBackend.controller.dto.GenericResponse;
 
-  protected HttpStatus status = HttpStatus.OK;
+public abstract class BaseController extends ResourceSupport {
 
-  protected static final ResponseEntity<Object> everythingIsFine =
-      new ResponseEntity<>(null, HttpStatus.OK);
+	protected HttpStatus status = HttpStatus.OK;
 
-  protected static final String BASE_URL = "/rest";
+	protected static final ResponseEntity<Object> everythingIsFine = new ResponseEntity<>(null, HttpStatus.OK);
+
+	protected static final String BASE_URL = "/rest";
+
+	protected GenericResponse createGenericResponse(Class controller) {
+		String name = "";
+		GenericResponse response = new GenericResponse(name);
+		
+		response.add(linkTo(methodOn(controller)).withSelfRel());
+		return response;
+	}
 
 }
