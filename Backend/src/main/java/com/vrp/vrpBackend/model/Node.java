0e8d7996d68dc137ef879b5b274caf39ae7090eb
lubiedak <l.biedak@wp.pl>
2015-11-22 15:05:05 +0000

    Adding Node class to model

---
+++ Backend/src/main/java/com/vrp/vrpBackend/model/Node.java
@@ -0,0 +1,55 @@
+package com.vrp.vrpBackend.model;
+
+public class Node {
+
+	private int id;
+	private String name;
+	private int x;
+	private int y;
+	private int demand;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getDemand() {
+		return demand;
+	}
+
+	public void setDemand(int demand) {
+		this.demand = demand;
+	}
+
+	@Override
+	public String toString() {
+		return "Node [id=" + id + ", name=" + name + ", x=" + x + ", y=" + y + ", demand=" + demand + "]";
+	}
+}
