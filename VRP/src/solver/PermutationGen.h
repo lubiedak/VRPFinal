9dbfdc5fa7fa075d4ed62b4a6f68d126f36cceb4
lubiedak <l.biedak@wp.pl>
2015-11-28 16:43:23 +0000

    Adding Permutation Generator

---
+++ VRP/src/solver/PermutationGen.h
@@ -0,0 +1,34 @@
+/*
+ * PermutationGen.h
+ *
+ *  Created on: 28 Nov 2015
+ *      Author: lubiedak
+ */
+
+#ifndef SOLVER_PERMUTATIONGEN_H_
+#define SOLVER_PERMUTATIONGEN_H_
+
+#include <vector>
+
+template <typename T>
+class PermutationGen {
+public:
+	PermutationGen();
+	PermutationGen(int size);
+	PermutationGen(std::vector<T> base);
+	virtual ~PermutationGen();
+
+	void Permute(int k, int size);
+
+private:
+
+	void Swap(int a, int b);
+	void Print(int size);
+
+
+	std::vector<T> base;
+	std::vector<T> perm;
+	std::vector< std::vector<T> > fullPermTable;
+};
+
+#endif /* SOLVER_PERMUTATIONGEN_H_ */
