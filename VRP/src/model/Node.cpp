75594c84e98783a1d3267b4f7586309e78b3659a
lubiedak <l.biedak@wp.pl>
2015-11-28 20:46:27 +0000

    Adding constructor to Node class

--- VRP/src/model/Node.cpp
+++ VRP/src/model/Node.cpp
@@ -8,12 +8,18 @@
 #include "Node.h"
 
 Node::Node() {
+	this->id = 0;
+	this->name = "Node0123";
+	this->x = 1;
+	this->y = 2;
+	this->demand = 3;
 }
 
 Node::~Node() {
 }
 
-Node::Node(unsigned id, std::string name, unsigned x, unsigned y, unsigned demand) {
+Node::Node(unsigned id, std::string name, unsigned x, unsigned y,
+		unsigned demand) {
 	this->id = id;
 	this->name = name;
 	this->x = x;
