c347541ff34c16fc0661a5dcb5de4bbee909a6b3
lubiedak <l.biedak@wp.pl>
2015-11-29 11:16:08 +0000

    Tests reorganization

--- VRP/src/test/Criteria_test.h
+++ VRP/src/test/Criteria_test.h
@@ -7,5 +7,14 @@
 
 #include "../model/Criteria.h"
 
+bool SerializationModel_TEST(bool silentMode){
+	Criteria cr = Criteria(1,2,3);
+	std::string expectedCR = "class=Criteria,maxCapacity=1,minCapacity=0"
+			",maxDistance=2,minDistance=0,maxNodes=3,minNodes=0";
 
+	std::cout<<expectedCR<<std::endl;
+	std::cout<<cr.toString()<<std::endl;
+
+	return 0 == expectedCR.compare(cr.toString());
+}
 
