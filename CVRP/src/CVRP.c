97c2bcceb2bec95d9888c6ec3de1f6040664b57f
lubiedak <l.biedak@wp.pl>
2015-12-05 12:28:52 +0000

    Simple CVRP-just C - init

---
+++ CVRP/src/CVRP.c
@@ -0,0 +1,28 @@
+/*
+ ============================================================================
+ Name        : CVRP.c
+ Author      : lubiedak
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "input.h"
+#include "structs.h"
+
+
+
+
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+
+
+
+
+	return EXIT_SUCCESS;
+}
