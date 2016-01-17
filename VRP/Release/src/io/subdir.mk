0c0382b0a4bb40a5fdaf1691249637ed8a92b03e
lubiedak <l.biedak@wp.pl>
2016-01-17 12:59:20 +0000

    Missing make files

---
+++ VRP/Release/src/io/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/io/ArgParser.cpp \
+../src/io/FileDirChecker.cpp 
+
+OBJS += \
+./src/io/ArgParser.o \
+./src/io/FileDirChecker.o 
+
+CPP_DEPS += \
+./src/io/ArgParser.d \
+./src/io/FileDirChecker.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/io/%.o: ../src/io/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O3 -Wall -c -fmessage-length=0 -std=c++0x -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
