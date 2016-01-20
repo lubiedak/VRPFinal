865b6194a6265e85e27d1cb5e8fd4242b5bbca61
Lukasz Biedak <l.biedak@wp.pl>
2016-01-20 16:13:02 +0000

    Adding parsing function to CriteriaDao

--- VRP/src/model/serialization/CriteriaDao.cpp
+++ VRP/src/model/serialization/CriteriaDao.cpp
@@ -6,6 +6,19 @@
  */
 
 #include "CriteriaDao.h"
+#include <iostream>
+
+uint16_t convertToInt(std::map<std::string, std::string>& params, std::string key){
+	uint16_t value = 0;
+	try{
+                value = (uint16_t)(std::stoi(params[key]));
+        }catch(const std::out_of_range& e){
+                std::cout<<" field "<<key<<" doesn't exist"<<std::endl;
+        }catch(const std::invalid_argument& e){
+                std::cout<<" field "<<key<<" is invalid"<<std::endl;
+        }
+	return value;
+}
 
 CriteriaDao::CriteriaDao(Criteria c) : criteria(c){
 	membersCount = 6;
@@ -22,5 +35,17 @@ std::string CriteriaDao::serialize() {
 bool CriteriaDao::deserialize(std::string str) {
 	std::map<std::string, std::string> values = parse(str);
 
+
+
+
+	uint16_t n = convertToInt(values,"minNodes");
+	uint16_t N = convertToInt(values,"maxNodes");
+        uint16_t c = convertToInt(values,"minCapacity");
+        uint16_t C = convertToInt(values,"maxCapacity");
+        uint16_t d = convertToInt(values,"minDistance");
+        uint16_t D = convertToInt(values,"maxDistance");
+	criteria = Criteria(C,D, N,c,d,n);
+
 	return true;
 }
+
