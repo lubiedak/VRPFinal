8a9961b4471208a312420b29fd73de30d3065715
lubiedak <l.biedak@wp.pl>
2016-02-01 07:59:06 +0000

    Basic onnecting to VRPCore

---
+++ Backend/src/main/java/com/vrp/vrpBackend/model/Problem.java
@@ -0,0 +1,21 @@
+package com.vrp.vrpBackend.model;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class Problem {
+	List<Node> nodes;
+	Criteria criteria;
+
+	@Override
+	public String toString() {
+		String str = "class=Problem\n";
+		str += criteria.toString() + "\n";
+		for (Node n : nodes) {
+			str += n.toString() + "\n";
+		}
+		return str;
+	}
+}
---
+++ Backend/src/main/java/com/vrp/vrpBackend/service/impl/VRPRunnerImpl.java
@@ -0,0 +1,45 @@
+package com.vrp.vrpBackend.service.impl;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import org.springframework.stereotype.Component;
+
+import com.vrp.vrpBackend.model.Problem;
+import com.vrp.vrpBackend.model.Solution;
+import com.vrp.vrpBackend.service.VRPRunner;
+
+@Component
+public class VRPRunnerImpl implements VRPRunner {
+
+	@Override
+	public String run(Problem p) {
+		String s = execute();
+		System.out.println(s);
+		return s;
+	}
+	
+	private String execute(){
+		StringBuffer output = new StringBuffer();
+		String command = "/home/lubiedak/workspace/VRPFinalRepo/VRP/Release/VRPCore2.0";
+
+		Process p;
+		try {
+			p = Runtime.getRuntime().exec(command);
+			p.waitFor();
+			BufferedReader reader = 
+                            new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+                        String line = "";			
+			while ((line = reader.readLine())!= null) {
+				output.append(line + "\n");
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return output.toString();
+	}
+
+}
