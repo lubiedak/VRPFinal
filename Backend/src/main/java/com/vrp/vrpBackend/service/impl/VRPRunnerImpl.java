6b9e049fec94dfe1b44696d066a408c88eb1b589
lubiedak <l.biedak@wp.pl>
2016-02-15 05:03:40 +0000

    Move cfg to yml file

--- Backend/src/main/java/com/vrp/vrpBackend/service/impl/VRPRunnerImpl.java
+++ Backend/src/main/java/com/vrp/vrpBackend/service/impl/VRPRunnerImpl.java
@@ -3,41 +3,44 @@ package com.vrp.vrpBackend.service.impl;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import com.vrp.vrpBackend.model.Problem;
 import com.vrp.vrpBackend.service.VRPRunner;
 
 @Component
+@ConfigurationProperties(prefix = "vrpRunner")
 public class VRPRunnerImpl implements VRPRunner {
 
+	String vrpPath;
+	
 	@Override
 	public String run(Problem p) {
 		String s = execute();
 		System.out.println(s);
+		System.out.println(p);
 		return s;
 	}
-	
-	private String execute(){
+
+	private String execute() {
 		StringBuffer output = new StringBuffer();
-		String command = "/home/lubiedak/workspace/VRPFinalRepo/VRP/Release/VRPCore2.0";
+		String command = vrpPath;
 
 		Process p;
 		try {
 			p = Runtime.getRuntime().exec(command);
 			p.waitFor();
-			BufferedReader reader = 
-                            new BufferedReader(new InputStreamReader(p.getInputStream()));
+			BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
 
-                        String line = "";			
-			while ((line = reader.readLine())!= null) {
+			String line = "";
+			while ((line = reader.readLine()) != null) {
 				output.append(line + "\n");
 			}
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
 		return output.toString();
 	}
 
