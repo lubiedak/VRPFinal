1c9b3784a774d677b59a1939f6c057787bbb6b64
lubiedak <l.biedak@wp.pl>
2016-03-04 08:00:36 +0000

    Remove everything connected to Mongo Repository

--- Backend/src/main/java/com/vrp/vrpBackend/dao/NodesDao.java
+++
@@ -1,8 +0,0 @@
-package com.vrp.vrpBackend.dao;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.vrp.vrpBackend.controller.dto.Nodes;
-
-public interface NodesDao extends MongoRepository<Nodes,String>{
-
-}
--- Backend/src/main/java/com/vrp/vrpBackend/service/NodeService.java
+++
@@ -1,8 +0,0 @@
-package com.vrp.vrpBackend.service;
-
-import com.vrp.vrpBackend.controller.dto.Nodes;
-
-public interface NodeService {
-
-	public Nodes save(Nodes nodes);
-}
