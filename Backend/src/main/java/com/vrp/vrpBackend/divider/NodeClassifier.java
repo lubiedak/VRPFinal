da2f2bd35ba8db86dc84a46fd562ea8c2f32b73e
lubiedak <l.biedak@wp.pl>
2016-07-04 21:20:07 +0200

    Adding NodesClassifier

---
+++ Backend/src/main/java/com/vrp/vrpBackend/divider/NodeClassifier.java
@@ -0,0 +1,23 @@
+package com.vrp.vrpBackend.divider;
+
+import com.vrp.vrpBackend.model.Node;
+
+public class NodeClassifier {
+	Node baseNode;
+	
+	public NodeClassifier(Node baseNode) {
+		this.baseNode = baseNode;
+	}
+
+	public double classify(Node n){
+		double x = n.getX() - baseNode.getX();
+		double y = n.getY() - baseNode.getY();
+		
+		double atan = Math.atan(x/y);
+		if(x < 0)
+			atan += Math.PI;
+		if(x > 0 && y < 0)
+			atan += 2*Math.PI;
+		return atan*180/Math.PI;
+	}
+}
---
+++ Backend/src/test/java/com/vrp/vrpBackend/divider/NodeClassifierTest.java
@@ -0,0 +1,57 @@
+package com.vrp.vrpBackend.divider;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.vrp.vrpBackend.model.Node;
+
+
+public class NodeClassifierTest {
+	
+	NodeClassifier classifier;
+	
+	@Before
+	public void setup(){
+		Node base = new Node("base");
+		base.setX(0);
+		base.setY(0);
+		classifier = new NodeClassifier(base);
+	}
+	
+	@Test
+	public void test1Quarter(){
+		Node n = createNode(1, 1);
+		double actual = classifier.classify(n);
+		Assert.assertEquals(45.0, actual,0.0001);
+	}
+	
+	@Test
+	public void test2Quarter(){
+		Node n = createNode(-1, 1);
+		double actual = classifier.classify(n);
+		Assert.assertEquals(135.0, actual,0.0001);
+	}
+	
+	@Test
+	public void test3Quarter(){
+		Node n = createNode(-1, -1);
+		double actual = classifier.classify(n);
+		Assert.assertEquals(225.0, actual,0.0001);
+	}
+	
+	@Test
+	public void test4Quarter(){
+		Node n = createNode(1, -1);
+		double actual = classifier.classify(n);
+		Assert.assertEquals(315.0, actual,0.0001);
+	}
+	
+	
+	private Node createNode(int x, int y){
+		Node n = new Node("N");
+		n.setX(x);
+		n.setY(y);
+		return n;
+	}
+}
