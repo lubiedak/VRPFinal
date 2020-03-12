4cc0eb2e816236abb5853ee0539fafe81f25c55f
lubiedak <l.biedak@wp.pl>
2020-03-12 08:26:04 +0100

    Add new Spring project

---
+++ forwarder/src/test/java/com/vrp/forwarder/ForwarderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vrp.forwarder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForwarderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
