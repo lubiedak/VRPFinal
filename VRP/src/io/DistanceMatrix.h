7d8028d347b1283d370c0d88b2f85767e0251722
lubiedak <l.biedak@wp.pl>
2020-03-31 12:07:42 +0200

    Add distance matrix

---
+++ VRP/src/io/DistanceMatrix.h
@@ -0,0 +1,21 @@
+/*
+ * DistanceMatrix.h
+ *
+ *  Created on: 31 Mar 2020
+ *      Author: lubiedak
+ */
+
+#ifndef IO_DISTANCEMATRIX_H_
+#define IO_DISTANCEMATRIX_H_
+
+#include <string>
+
+class DistanceMatrix {
+public:
+  DistanceMatrix() {}
+  DistanceMatrix(const std::string& fileName);
+
+private:
+
+};
+#endif
