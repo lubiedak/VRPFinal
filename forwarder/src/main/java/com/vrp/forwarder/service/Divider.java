4500e3724c814518ba7e348a50330762e1015eb2
lubiedak <l.biedak@wp.pl>
2020-04-16 13:08:18 +0200

    Dividing by group or depot

--- forwarder/src/main/java/com/vrp/forwarder/service/Divider.java
+++ forwarder/src/main/java/com/vrp/forwarder/service/Divider.java
@@ -1,15 +1,42 @@
 package com.vrp.forwarder.service;
 
+import com.vrp.forwarder.model.Criteria;
+import com.vrp.forwarder.model.Node;
 import com.vrp.forwarder.model.Problem;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Service
 public class Divider {
 
     private final static int BASE_GROUP_SIZE = 17;
+
+    public List<Problem> divideCitiesIntoProblems(List<Node> allCities) {
+        List<Node> cities = new ArrayList<>(allCities);
+        List<Node> depots = cities.stream()
+                                  .filter(Node::isDepot)
+                                  .collect(Collectors.toList());
+        cities.removeAll(depots);
+        Map<Node, List<Node>> map = depots.stream()
+                                          .collect(Collectors.toMap(Function.identity(),
+                                                                    depot -> cities.stream()
+                                                                                   .filter(c -> c.getGroup()
+                                                                                                 .equals(depot.getName()))
+                                                                                   .collect(Collectors.toList())));
+        return map.entrySet()
+                  .stream()
+                  .map(e -> Problem.builder()
+                                   .depot(e.getKey())
+                                   .nodes(e.getValue())
+                                   .criteria(new Criteria())
+                                   .build())
+                  .collect(Collectors.toList());
+    }
 
     public List<Problem> divide(Problem p){
         p.sortNodes();
